8aa574fca7359f6586e46d2c6fef6271f7218ade
Saksham Chawla <SAKSHAMCHAWLALKO@GMAIL.COM>
2025-10-15 16:37:32 +0530

    Update actions.ts

--- app/actions.ts
+++ app/actions.ts
@@ -2,7 +2,7 @@
 ..
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
-import { createToken, verifyToken } from "@/lib/authi"
+import { createToken, verifyToken } from "@/lib/auth"
 
 export async function login(username: string, password: string) {
   // Simple authentication - in a real app, you would check against a databasez
